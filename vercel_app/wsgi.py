74c98cc3197bbf61a9ba32cfa9945f70c1fc67ae
Per Olsen <persteen22@gmail.com>
2025-04-27 12:15:34 +0200

    Django Web App hosted at Vercel using MySQL

---
+++ vercel_app/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for vercel_app project.
+
+It exposes the WSGI callable as a module-level variable named ``app``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+
+from whitenoise import WhiteNoise
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+
+app = get_wsgi_application()
+
+app = WhiteNoise(app, root=os.path.join(os.path.dirname(__file__), 'static'))
+

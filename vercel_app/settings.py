381a05b08dbee5255b7be16b3829b76ab05a5b55
Per Olsen <persteen22@gmail.com>
2025-04-29 13:14:12 +0200

    Testing

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # Not: Django will display a 404.html if you have created one :-)
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['','127.0.0.1', '.vercel.app']
 
 
 # Application definition

978aea33daf6d80b31b5cf60f7b86cb33043535f
Per Olsen <persteen22@gmail.com>
2025-05-28 12:40:33 +0200

    Added the field Author to the Model Post

--- example/models.py
+++ example/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Post(models.Model):
+    
     title = models.CharField(max_length=100)
+    
+    # This field was added when the program was already in use and in production
+    author = models.CharField(max_length=100,default='Admin')
+
     content = models.TextField()
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

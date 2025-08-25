4f850db15ca78d6804611b4ea85e11033faf9911
Per Olsen <persteen22@gmail.com>
2025-08-25 08:25:57 +0200

    Added the Todo Model to the Admin Backend

--- example/admin.py
+++ example/admin.py
@@ -6,6 +6,10 @@ from example.models.post import Post
 
 from example.models.employee import Employee
 
+from example.models.todo import Todo
+
 admin.site.register(Post)
 
 admin.site.register(Employee)
+
+admin.site.register(Todo)

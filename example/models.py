af9e32f00f593fa5d57048123aff99602b6808e5
Per Olsen <persteen22@gmail.com>
2025-12-08 16:25:14 +0100

    Refactored msg in old views models and forms py

--- example/models.py
+++ example/models.py
@@ -1,18 +1,1 @@
-'''
-# 07-12-2025 - The Models are organized in the models folder for scalability
-from django.db import models
-
-class Post(models.Model):
-    
-    title = models.CharField(max_length=100)
-    
-    # This field was added when the program was already in use and in production
-    author = models.CharField(max_length=100,default='Admin')
-
-    content = models.TextField()
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-    '''
+# 08-12-2025 - The Models are organized in the models folder for scalability
